a5cfca47be8128c4274cecd79ed7e4a0f14b922b
HafizSp <hafizsp45@gmail.com>
2023-09-24 23:22:59 +0600

    ostad exam

--- lib/main.dart
+++ lib/main.dart
@@ -4,27 +4,38 @@ void main() {
   runApp(const CountApp());
 }
 
-class CountApp extends StatefulWidget {
+class CountApp extends StatelessWidget {
   const CountApp({super.key});
 
   @override
-  State<CountApp> createState() => _CountAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
 }
 
-class _CountAppState extends State<CountApp> {
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   customShowDialog(BuildContext context) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Congratulation!'),
-          content: Text('You\'ve bought 5 products'),
+          title: Text('Button pressed 5 times'),
+          // content: Text('You\'ve bought 5 products'),
           actions: [
             TextButton(
               onPressed: () {
                 Navigator.pop(context);
               },
-              child: Text('OK'),
+              child: Text('Cancel'),
             )
           ],
         );
@@ -32,31 +43,41 @@ class _CountAppState extends State<CountApp> {
     );
   }
 
+  int count = 0;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Counter App')),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Count:', style: TextStyle(fontSize: 20)),
-              Text('0', style: TextStyle(fontSize: 50)),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                      onPressed: () {},
-                      child: Text('+', style: TextStyle(fontSize: 30))),
-                  SizedBox(width: 20),
-                  ElevatedButton(
-                      onPressed: () {},
-                      child: Text('-', style: TextStyle(fontSize: 30))),
-                ],
-              )
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(title: Text('Counter App')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Count:', style: TextStyle(fontSize: 20)),
+            Text('$count', style: TextStyle(fontSize: 50)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      count++;
+                      setState(() {});
+                      if (count == 5) {
+                        customShowDialog(context);
+                      }
+                    },
+                    child: Text('+', style: TextStyle(fontSize: 30))),
+                SizedBox(width: 20),
+                ElevatedButton(
+                    onPressed: () {
+                      if (count != 0) {
+                        count--;
+                        setState(() {});
+                      }
+                    },
+                    child: Text('-', style: TextStyle(fontSize: 30))),
+              ],
+            )
+          ],
         ),
       ),
     );
